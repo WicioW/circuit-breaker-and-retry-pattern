5d776bbb8dc26fab9ef7d0e45beae079e35cbc16
wiktor <wwolakksiazek@fream.pl>
2022-06-18 19:33:47 +0200

    Updated gitignore and pom.xml files

---
+++ src/main/java/com/project/circuitbreakerandretrypattern/CircuitBreakerAndRetryPatternApplication.java
@@ -0,0 +1,13 @@
+package com.project.circuitbreakerandretrypattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerAndRetryPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircuitBreakerAndRetryPatternApplication.class, args);
+    }
+
+}
